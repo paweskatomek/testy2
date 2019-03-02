1786a6979333de3d2a78c468edfc2f27a936670c
Tomasz Pawęska <paweska.tomek@gmail.com>
2019-03-02 12:49:32 +0100

    drugi weekend cw

---
+++ src/main/java/pl/teb/edukacja/testy2/Testy2Application.java
@@ -0,0 +1,13 @@
+package pl.teb.edukacja.testy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testy2Application.class, args);
+	}
+
+}
