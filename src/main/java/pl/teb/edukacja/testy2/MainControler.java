b5ea11a69d3e97aa3625aebe4d2f0b3488b44e2d
Tomasz Pawęska <paweska.tomek@gmail.com>
2019-03-02 15:43:43 +0100

    drugi weekend cw

--- src/main/java/pl/teb/edukacja/testy2/MainControler.java
+++ src/main/java/pl/teb/edukacja/testy2/MainControler.java
@@ -1,6 +1,7 @@
 package pl.teb.edukacja.testy2;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class MainControler {
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World")
                                      String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
