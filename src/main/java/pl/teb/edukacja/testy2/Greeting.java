c9048b35c95275b0190ef424a080ce2d26cd47b9
Tomasz Pawęska <paweska.tomek@gmail.com>
2019-03-03 10:13:19 +0100

    dzień drugi

--- src/main/java/pl/teb/edukacja/testy2/Greeting.java
+++ src/main/java/pl/teb/edukacja/testy2/Greeting.java
@@ -1,19 +1,22 @@
 package pl.teb.edukacja.testy2;
 
 public class Greeting {
-    private final long id;
-    private final String content;
-
-    public Greeting(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
+    private long id;
+    private String content;
 
     public long getId() {
         return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getContent() {
         return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
--- src/main/java/pl/teb/edukacja/testy2/MainControler.java
+++ src/main/java/pl/teb/edukacja/testy2/MainControler.java
@@ -1,25 +1,30 @@
 package pl.teb.edukacja.testy2;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
+@Controller
 public class MainControler {
 
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World")
-                                     String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-
+    public String greeting(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
     }
 
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(
+            @ModelAttribute Greeting greeting) {
+        return "result";
+    }
 }
+
 
